aef1a7aa9332b359eaa4d1a2a595cd99d08e796e
kurt dev <73378023+djkurtttimajo@users.noreply.github.com>
2021-04-26 22:13:37 +0800

    initial commit

---
+++ js/index.js
@@ -0,0 +1,34 @@
+// const maindisplay = document.getElementById("main-content");
+
+// const home_section = document.getElementById("home");
+// const about_section = document.getElementById("about");
+
+// home_section.addEventListener("click",home);
+// about_section.addEventListener("click",about);
+
+// function home(){
+//     maindisplay.innerHTML = 
+//     `<div class="container content align-middle">
+//         <h1 class="we-create">We create <br> smart solutions. </h1>
+//         <hr align="left">
+//         <p><span class="ideas-tagline ">Kerrific Smarts</span> turns your ideas into reality at an affordable price.</p>
+//         <p><br><br></p>
+//     </div>  `
+// }
+
+// function about(){
+//     maindisplay.innerHTML = 
+//     `<div class="container d-flex justify-content-center content ">
+//         <div>
+            
+            
+//             <img class="mx-auto d-block" src="./images/Kerrific-name-logo.png" alt="name-logo" width="40%">
+//             <p class="text-center description ">
+//                 <span style="font-weight: bold;"> Kerrific Smarts</span> thinks, plans, designs, and develops smart digital solutions for your needs ranging from
+//                 information technology, software, education, and design.
+//             </p>        
+//         </div>    
+//     </div>`
+// }
+
+// home();
