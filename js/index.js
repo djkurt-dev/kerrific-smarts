07f6fa0d39ed204dad2716497dcc09256f6345b0
kurt dev <73378023+djkurtttimajo@users.noreply.github.com>
2021-04-27 17:03:10 +0800

    added transitions

--- js/index.js
+++ js/index.js
@@ -1,34 +1,25 @@
-// const maindisplay = document.getElementById("main-content");
+// scrollspy
+$('body').scrollspy({ target: '#navbarNavAltMarkup', offset: 10 });
 
-// const home_section = document.getElementById("home");
-// const about_section = document.getElementById("about");
+// $(window).scroll(function(){
+//     $('nav').toggleClass('scrolled',$(this).scrollTop()>80);
+// });
 
-// home_section.addEventListener("click",home);
-// about_section.addEventListener("click",about);
+$(window).on('scroll', function(){
+    if($(window).scrollTop()){
+        $('nav').addClass('bgcolor');
+        $('.kerrific').addClass('kerrific-small');
+        $('.smarts').addClass('smarts-small');
+        $('.nav-logo').addClass('nav-logo-small');
+        $('.nav-item').addClass('nav-item-small');
+    }
 
-// function home(){
-//     maindisplay.innerHTML = 
-//     `<div class="container content align-middle">
-//         <h1 class="we-create">We create <br> smart solutions. </h1>
-//         <hr align="left">
-//         <p><span class="ideas-tagline ">Kerrific Smarts</span> turns your ideas into reality at an affordable price.</p>
-//         <p><br><br></p>
-//     </div>  `
-// }
+    else{
+        $('nav').removeClass('bgcolor');
+        $('.kerrific').removeClass('kerrific-small');
+        $('.smarts').addClass('smarts-small');
+        $('.nav-logo').removeClass('nav-logo-small');
+        $('.nav-item').removeClass('nav-item-small');
+    }
+})
 
-// function about(){
-//     maindisplay.innerHTML = 
-//     `<div class="container d-flex justify-content-center content ">
-//         <div>
-            
-            
-//             <img class="mx-auto d-block" src="./images/Kerrific-name-logo.png" alt="name-logo" width="40%">
-//             <p class="text-center description ">
-//                 <span style="font-weight: bold;"> Kerrific Smarts</span> thinks, plans, designs, and develops smart digital solutions for your needs ranging from
-//                 information technology, software, education, and design.
-//             </p>        
-//         </div>    
-//     </div>`
-// }
-
-// home();
